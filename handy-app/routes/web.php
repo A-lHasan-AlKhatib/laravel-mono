d80e495a8632f6f519dbd5987a6714b42145b1d1
AlHasan AlKhatib <alhasan.ahmad.alkhatib@gmail.com>
2025-03-08 12:57:22 +0200

    - Use Route modle binfing to access a Job - create a Job Controller - use Route::view() for simple routes - use Route::resources() and use the JobController

--- handy-app/routes/web.php
+++ handy-app/routes/web.php
@@ -1,57 +1,8 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
 
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->simplePaginate(3);
-    return view('jobs.index', ['jobs' => $jobs]);
-});
-
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::query()->find($id);
-    return view('jobs.show', ['job' => $job]);
-});
-
-Route::get('/jobs/{id}/edit', function ($id) {
-    $job = Job::query()->find($id);
-    return view('jobs.edit', ['job' => $job]);
-});
-
-Route::patch('/jobs/{id}', function ($id) {
-    request()->validate(['title' => ['required', 'min:3'], 'salary' => ['required', 'min:3']]);
-    $job = Job::query()->findOrFail($id);
-    $job->update(['title' => request('title'), 'salary' => request('salary')]);
-    return redirect('/jobs/' . $job->id);
-});
-
-Route::delete('/jobs/{id}', function ($id) {
-    $job = Job::query()->findOrFail($id);
-    $job->delete();
-    return redirect('/jobs');
-});
-
-Route::post('/jobs', function () {
-    request()->validate(['title' => ['required', 'min:3'], 'salary' => ['required', 'min:3']]);
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 3 // Explicitly set employer_id
-    ]);
-
-    return redirect('/jobs');
-});
-
-
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::view('/', 'home');
+Route::view('contact', 'contact');
+Route::resource('jobs', JobController::class);

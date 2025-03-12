0f8ed4fc27f92a81b6c202d271afca73c73f5e17
AlHasan AlKhatib <alhasan.ahmad.alkhatib@gmail.com>
2025-03-12 17:02:02 +0200

    Design main page for pixel jobs, TODO: Design extended job card

---
+++ pixel-positions/resources/views/components/job-card.blade.php
@@ -0,0 +1,17 @@
+<div class="p-4 bg-white/10 rounded-xl flex-col text-center">
+    <div class="self-start text-sm">
+        Employer
+    </div>
+    <div class="py-8 font-bold ">
+        <h3>Job Title</h3>
+        <p>Job details and salary</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+        <img src="https://placehold.co/42" alt="place holder">
+    </div>
+</div>
---
+++ pixel-positions/resources/views/components/layout.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Pixel Positions</title>
+    @vite(['resources/js/app.js'])
+</head>
+<body class="bg-black text-white">
+<div class="px-10">
+    <nav class="flex justify-between items-center">
+        <div>
+            <a href="/">
+                <img src="{{Vite::asset('resources/images/logo.svg')}}" alt="pixel positions logo">
+            </a>
+        </div>
+        <div class="space-x-6 font-bold">
+            <a href="">Jobs</a>
+            <a href="">Careers</a>
+            <a href="">Salaries</a>
+            <a href="">Companies</a>
+        </div>
+        <div><a href="">post a job</a></div>
+    </nav>
+
+    <main class="mt-10 max-w-[986px] mx-auto">
+        {{$slot}}
+    </main>
+</div>
+</body>
+</html>
